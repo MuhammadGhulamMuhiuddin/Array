e9e68eabcb829f4e037f46d84a798001e55a493b
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-09-20 09:20:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,158 @@
+// Question No.01
+
+// var arrayOfArray;
+// arrayOfArray = [];
+
+// Question No. 02
+//  arrayOfArray = [
+//     [1, 2, 3], 
+//     [4, 5, 6], 
+//     [7, 8, 9] 
+// ];
+
+// document.write(arrayOfArray[0] , "<br>");
+// document.write(arrayOfArray[1] , "<br>");
+// document.write(arrayOfArray[2] , "<br>");
+
+
+
+// Question No. 03
+
+// var i = 1;
+// for (i = 1; i <= 10; i++) {
+//    document.write("<h1>" + i + "</h1>")
+// }
+
+// Question No. 04
+
+// var table = +prompt("Enter a number to show its multiplication table");
+
+ 
+// for(var table = 1; table <= 20; table++){
+//     document.write("2  x " + i + " = " + 2 * i + "<br/>")
+// }
+
+
+// Question N0. 05
+
+ 
+ 
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+// var fruits = ["“apple”", "“banana”", "“mango”", "“orange”","“strawberry”"];
+// for(var i = 0; i < fruits.length; i){
+// console.log(fruits[i]);
+// }
+
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "<br/>");
+
+    
+    
+// }
+
+
+
+// Question No. 06
+
+//a
+
+// var counting;
+// counting = [1,2,3,4,5,6,7,8,10,11,12,13,14,15];
+// document.write("<h1>" + Counting + "</h1>");
+// document.write(counting);
+
+//b
+
+
+// var reverseCounting;
+// reverseCounting = [1,2,3,4,5,6,7,8,9,10];
+// document.write("<h1>" + Reverse + Counting + "</h1>");
+// var result = reverseCounting.reverse()
+// document.write(reverseCounting);
+
+// c
+
+// var even;
+// even = [0,9,8,7,6,5,4,3,2,1];
+// document.write("<h1>" + Even + "</h1>");
+// var result = even.short();
+// document.write(result);
+
+// d
+
+// odd = [19,17,15,13,11,9,7,5,3,1];
+// document.write("<h1>" + Odd + "</h1>");
+// var result = odd.short();
+// document.write(result);
+
+// e
+
+// series = ["20k, 18k, 16k, 14k, 12k, 10k, 8k, 6k, 4k, 2k"];
+// document.write("<h1>" + Series + "</h1>");
+// var result = series.short();
+// document.write(result);
+
+
+// Question No. 07
+
+// var A = ["cake", "apple" ,"pie", "cookie", "chips", "patties"];
+// var userCity = prompt("Hey! What you want");
+// var flag = false;
+
+// for (var i = 0; i < A.length; i++) {
+//   if (A[i] === userCity) {
+//     flag = true;
+//     break;
+//   }
+// }
+
+// if (flag) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+//     if (i === 5) {
+//         break;
+//     }
+// }
+
+
+// Question No. 08
+ 
+// var num = [24, 53, 78, 91, 12];
+
+// console.log(A[4]);
+
+// Question No. 09
+ 
+// var num = [24, 53, 78, 91, 12];
+
+// console.log(A[0]);
+   
+// Question No. 10 
+//  var i;
+// for (i = 1; i <= 100; i++) {
+  
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+ 
+ 
+
+
